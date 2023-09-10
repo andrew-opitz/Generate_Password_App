bba45657da504cbe71516e162e0e1aa03acfcdc2
andrew.opitz222@gmail.com <andrew.opitz@yahoo.com>
2023-09-10 17:30:29 -0400

    defined character options

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lower = ('abcdefghijklmnopqrstuvwxyz')
+var upper = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+var numbers = ('123456789')
+var specials = ('`~!@#$%^&-_<>?')
+var combined = ('')
+var password = ('')
 
+var Lowercase = confirm('Would you like to inlcude lowercase characters?')
+var Uppercase = confirm('Would you like to include uppercase characters?')
+var Numbers = confirm('Would you like to include numbers?')
+var Specials = confirm('Would you like to include special characters?')
 
 function generatePassword() {
-
-
+  
+  
   return 
 }
 
@@ -12,9 +22,9 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
